bbd886f8d3c05cf7de269a748031a1405a5a4c14
chakkun1121 <chakkun1121haruki@gmail.com>
2023-01-17 18:36:34 +0900

    add machine

---
+++ machine/script.js
@@ -0,0 +1,8 @@
+const CARD_ELEMENT_MAX = 75;
+let cardElements = [...Array(CARD_ELEMENT_MAX + 1)].map((_, i) => i);
+//ビンゴカードの要素の最大値
+function startBingo() {
+  //シャッフルして何を出すか決める
+  //出した後、cardElementsから撤去
+  //履歴欄にも追加
+}
