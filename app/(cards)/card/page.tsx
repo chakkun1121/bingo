2d771a97ccd75e2920cc3d6fad757adec1ba9861
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-01 23:32:17 +0000

    スタイルの調節

--- app/(cards)/card/page.tsx
+++ app/(cards)/card/page.tsx
@@ -8,30 +8,40 @@ export const metadata: Metadata = {
     canonical: "/card",
   },
 }
-export default function Card() {
+export default function Main() {
   const createCard = useCreateCard();
   const card: bingoCardType = createCard();
   return (
     <>
       <section>
-        <h1>ビンゴ</h1>
-        <div className="text-center grid grid-rows-5 gap-4 max-w-2xl">
-          <div className="grid grid-cols-5 gap-4 ">{["B", "I", "N", "G", "O"].map((item) => (
-            <p className="text-heading-L" key={item}>{item}</p>
-          ))}</div>
-          {card.map((cols: number[], i: number) => (
-            <div key={i} className="grid grid-cols-5 gap-4 ">
-              {cols.map((num: number, j: number) => (
-                <div key={j} className="bg-gray-100 p-2">
-                  <p className="text-heading-L">
-                    {typeof num === "number" ? num : "FREE"}
-                  </p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+        <h1>ビンゴカード</h1>
+        <BingoCard card={card} />
       </section>
     </>
   );
+}
+export function BingoCard({ card, imagePath = "../img/headerLogo.png", iconPosition = "left" }: { card: bingoCardType, imagePath?: string, iconPosition?: "left" | "center" | "right" }) {
+  return (
+    <div className="p-2 border mx-auto w-max">
+      <img className={`max-w-40 max-h-40 items-${iconPosition}`} src={imagePath} />
+      <div className="text-center grid grid-rows-5 gap-4 max-w-2xl">
+        <div className="grid grid-cols-5 gap-4 ">
+          {["B", "I", "N", "G", "O"].map((item) => (
+            <p className="text-heading-L" key={item}>{item}</p>
+          ))}
+        </div>
+        {card.map((cols: number[], i: number) => (
+          <div key={i} className="grid grid-cols-5 gap-4 ">
+            {cols.map((num: number, j: number) => (
+              <div key={j} className="bg-gray-100 p-2">
+                <p className="text-heading-L">
+                  {typeof num === "number" ? num : "FREE"}
+                </p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
