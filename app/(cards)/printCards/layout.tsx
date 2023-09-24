46064db7e9a10ee4ac88af41ab3cada343d774f5
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-25 08:45:51 +0900

    メタタグの修正

---
+++ app/(cards)/printCards/layout.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ビンゴカードをまとめて印刷",
+  description:
+    "ビンゴカードをまとめて印刷するページです。イベントの準備などの際にご利用ください。",
+};
+
+export default function Layout({ children }) {
+  return children;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,21 @@ export default function Main() {
       <section className="bg-green-100 rounded p-2 m-2">
         <h2>アプリの中身</h2>
         <ul className="list-disc pl-4 list-inside">
-          <li><Link href="./card">ビンゴカード</Link></li>
-          <li><Link href="./machine/">ビンゴマシーン</Link></li>
-          <li><Link href="./printCards/">ビンゴカードをまとめて印刷</Link></li>
-          <li><Link href="./settings/">設定</Link></li>
-          <li><Link href="./help">このアプリについて</Link></li>
+          <li>
+            <Link href="./card">ビンゴカード</Link>
+          </li>
+          <li>
+            <Link href="./machine/index.html">ビンゴマシーン</Link>
+          </li>
+          <li>
+            <Link href="./printCards">ビンゴカードをまとめて印刷</Link>
+          </li>
+          <li>
+            <Link href="./settings/">設定</Link>
+          </li>
+          <li>
+            <Link href="./help">このアプリについて</Link>
+          </li>
         </ul>
       </section>
     </>
