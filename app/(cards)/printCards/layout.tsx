6cb537162825ba95f0658d761d68c8262c537725
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-10-06 10:47:30 +0900

    Update layout.tsx

--- app/(cards)/printCards/layout.tsx
+++ app/(cards)/printCards/layout.tsx
@@ -4,6 +4,9 @@ export const metadata: Metadata = {
   title: "ビンゴカードをまとめて印刷",
   description:
     "ビンゴカードをまとめて印刷するページです。イベントの準備などの際にご利用ください。",
+  alternates: {
+    canonical: "/printCards",
+  },
 };
 
 export default function Layout({ children }) {
