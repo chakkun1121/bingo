e627cef36ee69d1617a77a7e8722fcf0b6a0a84d
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-12-18 23:15:05 +0000

    カードのスマホ対応

--- app/(cards)/_components/BingoCard.tsx
+++ app/(cards)/_components/BingoCard.tsx
@@ -21,51 +21,53 @@ export function BingoCard({
     }) {
   const settings = useRecoilValue(settingsAtom);
   return (
-    <div
-      className="p-2 border mx-auto w-max select-none print:block"
-      style={{ pageBreakAfter: "always" }}
-    >
-      <img
-        className={`max-w-40 max-h-40 items-${settings.printCardHeaderIconPosition}`}
-        src={settings.printCardHeaderIcon}
-        alt="カードのロゴ"
-      />
-      <div className="text-center grid grid-rows-5 gap-4 max-w-2xl">
-        <div className="grid grid-cols-5 gap-4 ">
-          {["B", "I", "N", "G", "O"].map((item) => (
-            <p className="text-heading-L" key={item}>
-              {item}
-            </p>
-          ))}
-        </div>
-        {card.map((cols: number[], i: number) => (
-          <div key={i} className="grid grid-cols-5 gap-4 ">
-            {cols.map((num: number, j: number) => (
-              <button
-                key={j}
-                className={`bg-gray-100  text-L font-normal aspect-square ${
-                  cardInfo?.[i * 5 + j] &&
-                  "rounded-t-full bg-gray-900 text-gray-500"
-                }`}
-              >
-                <p
-                  className={`text-heading-L p-2`}
-                  onClick={() => {
-                    if (setCardInfo) {
-                      setCardInfo(
-                        cardInfo.map((item, index) =>
-                          index === i * 5 + j ? !item : item
-                        ) as unknown as arrayExactLength<boolean, 25>
-                      );
-                    }
-                  }}
-                >
-                  {typeof num === "number" ? num : "FREE"}
-                </p>
-              </button>
+    <div className="flex flex-col items-center">
+      <div
+        className="p-2 border select-none print:block"
+        style={{ pageBreakAfter: "always" }}
+      >
+        <img
+          className={`max-w-40 max-h-40 items-${settings.printCardHeaderIconPosition}`}
+          src={settings.printCardHeaderIcon}
+          alt="カードのロゴ"
+        />
+        <div className="text-center grid grid-rows-5 gap-4 max-w-2xl">
+          <div className="grid grid-cols-5 gap-4 ">
+            {["B", "I", "N", "G", "O"].map((item) => (
+              <p className="text-heading-L" key={item}>
+                {item}
+              </p>
             ))}
           </div>
-        ))}
+          {card.map((cols: number[], i: number) => (
+            <div key={i} className="grid grid-cols-5 gap-4 ">
+              {cols.map((num: number, j: number) => (
+                <button
+                  key={j}
+                  className={`bg-gray-100  text-L font-normal aspect-square flex items-center justify-center ${
+                    cardInfo?.[i * 5 + j] &&
+                    "rounded-t-full bg-gray-900 text-gray-500"
+                  }`}
+                >
+                  <p
+                    className={`text-[min(5vw,36px)] p-2`}
+                    onClick={() => {
+                      if (setCardInfo) {
+                        setCardInfo(
+                          cardInfo.map((item, index) =>
+                            index === i * 5 + j ? !item : item
+                          ) as unknown as arrayExactLength<boolean, 25>
+                        );
+                      }
+                    }}
+                  >
+                    {typeof num === "number" ? num : "FREE"}
+                  </p>
+                </button>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
