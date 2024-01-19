3e38db9070f42960263c07c5231d326fd8bc3d5b
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-01-19 19:03:07 +0900

    Update page.tsx
    文章を修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 import React from "react";
 export default function Main() {
   return (
-    <>
       <section className="bg-green-100 rounded p-2 m-2">
-        <h2>アプリの中身</h2>
+        <h2>このアプリについて</h2>
+        <p>
+          このアプリはweb上でビンゴを行うのに必要なものがすべて揃っています。
+          スマホ対応のビンゴカード、ビンゴマシーンとイベントの準備に役立つビンゴカード一斉作成機能があります。
+        </p>
+      <section>
+      <section className="bg-green-100 rounded p-2 m-2">
+        <h2>このアプリのそれぞれの機能へのリンク</h2>
         <ul className="list-disc pl-4 list-inside">
           {[
             {
